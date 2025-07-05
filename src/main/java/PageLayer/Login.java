a93693a2631f8db978c323a913edee556dbee21d
GOWRI <GOWRI@DESKTOP-EROAKQK>
2025-07-05 16:06:34 -0700

    Framework created by gowri teamlead....

---
+++ src/main/java/PageLayer/Login.java
@@ -0,0 +1,12 @@
+package PageLayer;
+
+public class Login {
+
+	public void loginfunctionality(String username, String password)
+	{
+		System.out.println("Username:   "+username);
+		System.out.println("Password:   "+password);
+
+		System.out.println("User logged in successfully ");
+	}
+}
---
+++ src/test/java/TestLayer/test.java
@@ -0,0 +1,12 @@
+package TestLayer;
+
+import PageLayer.Login;
+
+public class test {
+
+	public static void main(String[] args) {
+		Login log=new Login();
+		log.loginfunctionality("Admin", "admin123");
+	}
+
+}
