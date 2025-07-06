30bc7b920416e12c705eee8c13e47fa6e5d12e4b
GOWRI <GOWRI@DESKTOP-EROAKQK>
2025-07-05 17:12:42 -0700

    Home Page Title Validated by Tester11

--- src/main/java/PageLayer/Home.java
+++ src/main/java/PageLayer/Home.java
@@ -4,6 +4,10 @@ public class Home {
 	public void homepageFunctionality()
 	{
 		System.out.println("Home page logo Validated");
+		
+		
+		
+		System.out.println("Home page Title validated");
 	}
 
 }
